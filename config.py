59647f6364c1518e6cccc6c69938a458b488851a
aaronfraint <afraint@dvrpc.org>
2020-07-23 18:22:10 +0000

    adding config file

---
+++ config.py
@@ -0,0 +1,21 @@
+"""Flask app configuration."""
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
+class Config:
+    """Set Flask configuration from environment variables."""
+
+    SECRET_KEY = environ.get('SECRET_KEY')
+
+    # Static Assets
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+
+    # Flask-SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
