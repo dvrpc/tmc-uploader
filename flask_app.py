389ed08f12dc945106717dca4c49f3920d3bc060
aaronfraint <afraint@dvrpc.org>
2020-07-23 21:42:23 +0000

    updates to play nicely with PythonAnywhere

--- flask_app.py
+++ flask_app.py
@@ -1,20 +1,30 @@
 """ Setup the Flask application """
+import os
 from flask import Flask
-from flask_login import LoginManager
-from dotenv import load_dotenv, find_dotenv
 
 from db import db
 from routes.auth import login_manager
+from sql_connection import SQLALCHEMY_DATABASE_URI
 
-load_dotenv(find_dotenv())
+# General Config
+DEBUG = True
+SECRET_KEY = os.urandom(32)
 
+# Database
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Create the app
 app = Flask(__name__)
 
 # Pass configuration values
-app.config.from_object("config.Config")
+app.config["DEBUG"] = DEBUG
+app.config["SECRET_KEY"] = SECRET_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_ECHO"] = SQLALCHEMY_ECHO
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = SQLALCHEMY_TRACK_MODIFICATIONS
 
+db.init_app(app)
 login_manager.init_app(app)
 
 with app.app_context():
@@ -26,7 +36,7 @@ with app.app_context():
 
 if __name__ == "__main__":
 
-    db.init_app(app)
+
     with app.app_context():
         db.create_all()
     app.run(debug=True)
