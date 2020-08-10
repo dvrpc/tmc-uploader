2490b60b91ac10306b658fab6e81ba2243558f92
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-08-10 15:36:48 -0700

    assign files to project

--- forms/assign_files_to_project.py
+++ forms/assign_files_to_project.py
@@ -5,3 +5,7 @@ from wtforms import IntegerField
 class AssignFilesToProject(FlaskForm):
 
     file_id = IntegerField("File ID")
+
+
+class AssignProjectsToFile(FlaskForm):
+    project_id = IntegerField("Project ID")
