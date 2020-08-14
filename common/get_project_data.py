192596bf3e37367d7ea6f44fe788a2715d27c05c
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-08-13 17:07:56 -0700

    make total column a parameter

--- common/get_project_data.py
+++ common/get_project_data.py
@@ -1,7 +1,5 @@
 """
 Merge the already-uploaded data for all files assigned to a given project
-
-
 """
 import pandas as pd
 from sqlalchemy import create_engine
@@ -11,8 +9,20 @@ from models import Project
 from sql_connection import SQLALCHEMY_DATABASE_URI
 
 
-def project_df(project_id: int, uri: str = SQLALCHEMY_DATABASE_URI):
+def project_df(project_id: int,
+               total_colname: str = "total_15_min",
+               uri: str = SQLALCHEMY_DATABASE_URI):
+    """
+    Generate a dataframe for a given project ID:
+        - query to get this Project's object
+        - get a list of all files assigned to the project
+        - pull a dataframe from SQL for each individual file
 
+    Options:
+        - choose which total column to use: total_15_min or total_hourly
+
+    Returns a list of dataframes that you'll need to concat.
+    """
     this_project = Project.query.filter_by(uid=project_id).first()
 
     file_ids = [f.uid for f in this_project.tmc_files]
@@ -24,8 +34,10 @@ def project_df(project_id: int, uri: str = SQLALCHEMY_DATABASE_URI):
         q = f"""
             SELECT
                 to_char(datetime, 'HH24:MI') as time_idx,
-                CASE WHEN f.title IN (NULL, '') THEN f.filename ELSE f.title END AS location,
-                total_15_min
+                CASE WHEN f.title IN (NULL, '')
+                        THEN f.filename
+                     ELSE f.title END AS location,
+                {total_colname}
             FROM tmc_data.tmc_{fid}
             LEFT JOIN filedata f on f.uid = {fid}
             """
@@ -39,4 +51,4 @@ def project_df(project_id: int, uri: str = SQLALCHEMY_DATABASE_URI):
 
         all_dfs.append(df)
 
-    return all_dfs #pd.concat(all_dfs)
+    return all_dfs
